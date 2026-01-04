1ac8994695c259bd520072a5113b1b71f16e6a9e
Birana19 <mahinbiranu@gmail.com>
2026-01-04 23:18:59 +0530

    update day4

--- script.js
+++ script.js
@@ -4,9 +4,27 @@ let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
+let moves = 0;
+let score = 0;
+let matchedPairs = 0;
+let timer = 0;
+let timerInterval;
+
+const movesEl = document.getElementById('moves');
+const scoreEl = document.getElementById('score');
+const timerEl = document.getElementById('timer');
+
+
 function flipCard() {
     if (lockBoard) return;
     if (this === firstCard) return;
+
+    if (!timerInterval) {
+        timerInterval = setInterval(() => {
+            timer++;
+            timerEl.textContent = timer;
+        }, 1000);
+    }
 
     this.classList.add('flip');
 
@@ -21,6 +39,8 @@ function flipCard() {
 }
 
 function checkForMatch() {
+    moves++;
+    movesEl.textContent = moves;
     const isMatch =
         firstCard.dataset.framework === secondCard.dataset.framework;
 
@@ -30,6 +50,14 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    matchedPairs++;
+    score += 100;
+    scoreEl.textContent = score;
+
+    if (matchedPairs === cards.length / 2){
+        endGame();
+    }
     resetBoard();
 }
 
@@ -57,3 +85,42 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+function endGame(){
+    clearInterval(timerInterval);
+
+    document.getElementById('finalTime').textContent = timer;
+    document.getElementById('finalMoves').textContent = moves;
+    document.getElementById('finalScore').textContent = score;
+    
+    document.getElementById('winModal').style.display = 'flex';
+}
+
+document.getElementById('restart').addEventListener('click', restartGame);
+
+function restartGame(){
+    clearInterval(timerInterval);
+    timerInterval = null;
+    timer = 0;
+    moves = 0;
+    score = 0;
+    matchedPairs = 0;
+
+    timerEl.textContent = 0;
+    movesEl.textContent = 0;
+    scoreEl.textContent = 0;
+
+    document.getElementById('winModal').style.display ='none';
+
+    cards.forEach(card => {
+        card.classList.remove('flip');
+        card.addEventListener('click', flipCard);
+        card.style.order = Math.floor(Math.random() * cards.length);
+    });
+}
+
+
+
+
+
